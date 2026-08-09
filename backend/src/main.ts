b923fc037052496ef0b8aa0d6eacbfdd33b6332b
Parit-III <parit.rungrueang@gmail.com>
2026-08-09 10:58:31 +0700

    add root dir in tsconfig

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,9 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  });
+  origin: [
+    process.env.FRONTEND_URL,       // https://parit-ru.github.io
+    "http://localhost:5173",        // your local Vite dev server
+  ],
+  credentials: true,
+});
 
   await app.listen(process.env.PORT ?? 3000);
 }
